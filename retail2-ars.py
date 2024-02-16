b1ef0fdad54f282f1f315a1b37269f096acd97c2
Mehmet Kutay Dündar <93616716+mehmetkutay07@users.noreply.github.com>
2024-02-16 05:35:54 +0300

    Add files via upload

---
+++ retail2-ars.py
@@ -0,0 +1,253 @@
+############################################
+# ASSOCIATION RULE LEARNING (BİRLİKTELİK KURALI ÖĞRENİMİ)
+############################################
+
+# 1. Veri Ön İşleme
+# 2. ARL Veri Yapısını Hazırlama (Invoice-Product Matrix)
+# 3. Birliktelik Kurallarının Çıkarılması
+# 4. Çalışmanın Scriptini Hazırlama
+# 5. Sepet Aşamasındaki Kullanıcılara Ürün Önerisinde Bulunmak
+
+############################################
+# 1. Veri Ön İşleme
+############################################
+
+# !pip install mlxtend
+import pandas as pd
+pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 500)
+# çıktının tek bir satırda olmasını sağlar.
+pd.set_option('display.expand_frame_repr', False)
+from mlxtend.frequent_patterns import apriori, association_rules
+
+# https://archive.ics.uci.edu/ml/datasets/Online+Retail+II
+
+df_ = pd.read_excel("datasets/online_retail_II.xlsx",
+                    sheet_name="Year 2010-2011")
+df = df_.copy() # Veri setini koruyarak işlem yapmak için bir kopyası oluşturulur.
+df.head()
+# pip install openpyxl
+# df_ = pd.read_excel("datasets/online_retail_II.xlsx",
+#                     sheet_name="Year 2010-2011", engine="openpyxl")
+
+
+df.describe().T   # Sayısal değişkenlerin istatistiklerini al
+df.isnull().sum() # Değişkenlerdeki boş değerleri göster
+df.shape          # Veri setinin boyut bilgisini getir
+
+def retail_data_prep(dataframe):
+    dataframe.dropna(inplace=True)                                           # Veri setindeki boş değerleri veri setinde çıkarır.Inplace parametresi ile bu işlem kalıcı hale getirilir.
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)] # İptal edilen faturaları veri setinden çıkarır.
+    dataframe = dataframe[dataframe["Quantity"] > 0]                         # Miktarı 0'dan az olan ürünleri çıkarır.
+    dataframe = dataframe[dataframe["Price"] > 0]                            # Fiyatı 0'dan az olan ürünleri çıkarır.
+    return dataframe
+
+df = retail_data_prep(df)
+
+
+# Eşik değer belirleme (Aykırı değer)
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01) # Yüzde 1'lik çeyrek değer
+    quartile3 = dataframe[variable].quantile(0.99) # Yüzde 99'luk çeyrek değer
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit # Belirlenen değişkende low limit'ten düşük olanları low limit ile yer değiştirir.
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit   # Belirlenen değişkende up limit'ten yüksek olanları up limit ile yer değiştirir.
+
+def retail_data_prep(dataframe):
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    return dataframe
+
+df = retail_data_prep(df)
+df.isnull().sum()
+df.describe().T
+
+
+############################################
+# 2. ARL Veri Yapısını Hazırlama (Invoice-Product Matrix)
+############################################
+
+df.head()
+
+# Description   NINE DRAWER OFFICE TIDY   SET 2 TEA TOWELS I LOVE LONDON    SPACEBOY BABY GIFT SET
+# Invoice
+# 536370                              0                                 1                       0
+# 536852                              1                                 0                       1
+# 536974                              0                                 0                       0
+# 537065                              1                                 0                       0
+# 537463                              0                                 0                       1
+
+
+df_fr = df[df['Country'] == "France"]
+
+df_fr.groupby(['Invoice', 'Description']).agg({"Quantity": "sum"}).head(20)
+
+df_fr.groupby(['Invoice', 'Description']).agg({"Quantity": "sum"}).unstack().iloc[0:5, 0:5]           # Ürün isimlendirmelerini değişken isimlendirmesine çevirir
+
+df_fr.groupby(['Invoice', 'Description']).agg({"Quantity": "sum"}).unstack().fillna(0).iloc[0:5, 0:5] # Faturada bulunmayab ürüne "NaN" değil 0 yazdır.
+
+# Ürün isimleri yerine ürün ID'leri getirilir.(StockCode)
+df_fr.groupby(['Invoice', 'StockCode']). \
+    agg({"Quantity": "sum"}). \
+    unstack(). \
+    fillna(0). \
+    applymap(lambda x: 1 if x > 0 else 0).iloc[0:5, 0:5] # Eğer faturada üründen varsa ise 1, yoksa 0 yaz.
+# applymap fonksiyonu bütün gözlemleri gezer.
+
+# Stock Code veya Descriptiona göre getirme (Seçme)
+def create_invoice_product_df(dataframe, id=False):
+    if id:
+        return dataframe.groupby(['Invoice', "StockCode"])['Quantity'].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+    else:
+        return dataframe.groupby(['Invoice', 'Description'])['Quantity'].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+
+fr_inv_pro_df = create_invoice_product_df(df_fr)
+
+fr_inv_pro_df = create_invoice_product_df(df_fr, id=True)
+
+
+def check_id(dataframe, stock_code):
+    product_name = dataframe[dataframe["StockCode"] == stock_code][["Description"]].values[0].tolist()
+    print(product_name)
+
+
+check_id(df_fr, 10120)
+
+############################################
+# 3. Birliktelik Kurallarının Çıkarılması
+############################################
+
+# Apriori metodu veri setinde birliktelik kurallarını tespit eder.
+frequent_itemsets = apriori(fr_inv_pro_df,
+                            min_support=0.01,
+                            use_colnames=True)
+
+frequent_itemsets.sort_values("support", ascending=False)
+
+rules = association_rules(frequent_itemsets,
+                          metric="support",
+                          min_threshold=0.01)
+
+# Antecedent:ilk ürün
+# Consquent: ikinci ürün
+
+rules[(rules["support"]>0.05) & (rules["confidence"]>0.1) & (rules["lift"]>5)]
+
+check_id(df_fr, 21086)
+
+rules[(rules["support"]>0.05) & (rules["confidence"]>0.1) & (rules["lift"]>5)]. \
+sort_values("confidence", ascending=False)
+
+############################################
+# 4. Çalışmanın Scriptini Hazırlama
+############################################
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+def retail_data_prep(dataframe):
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    return dataframe
+
+
+def create_invoice_product_df(dataframe, id=False):
+    if id:
+        return dataframe.groupby(['Invoice', "StockCode"])['Quantity'].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+    else:
+        return dataframe.groupby(['Invoice', 'Description'])['Quantity'].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+
+
+def check_id(dataframe, stock_code):
+    product_name = dataframe[dataframe["StockCode"] == stock_code][["Description"]].values[0].tolist()
+    print(product_name)
+
+
+def create_rules(dataframe, id=True, country="France"):
+    dataframe = dataframe[dataframe['Country'] == country]
+    dataframe = create_invoice_product_df(dataframe, id)
+    frequent_itemsets = apriori(dataframe, min_support=0.01, use_colnames=True)
+    rules = association_rules(frequent_itemsets, metric="support", min_threshold=0.01)
+    return rules
+
+df = df_.copy()
+
+df = retail_data_prep(df)
+rules = create_rules(df)
+
+rules[(rules["support"]>0.05) & (rules["confidence"]>0.1) & (rules["lift"]>5)]. \
+sort_values("confidence", ascending=False)
+
+############################################
+# 5. Sepet Aşamasındaki Kullanıcılara Ürün Önerisinde Bulunmak
+############################################
+
+# Örnek:
+# Kullanıcı örnek ürün id: 22492
+
+product_id = 22492
+check_id(df, product_id)
+
+sorted_rules = rules.sort_values("lift", ascending=False) # Kuralları lift'e göre büyükten küçüğe sıraladık.
+
+recommendation_list = []
+# Belirlenen ürünü ilk ürün(antecedents) kombinasyonlarında ara bulduğunda indexini getir,daha sonrasında o indexteki ikinci ürünü(consequents) bana ver.
+for i, product in enumerate(sorted_rules["antecedents"]): # Seçtiğimiz ürünü,kombinasyonlarda bul.(antecendetste gezer)
+    for j in list(product): # Bulduğumuz ürün grubunu listeye çeviriyoruz
+        if j == product_id: # Eğer j seçilen ürün id'sine eşit ise
+            recommendation_list.append(list(sorted_rules.iloc[i]["consequents"])[0]) # İkinci ürünü (consequents) recommendations'a ekle.
+
+recommendation_list[0:3]
+
+check_id(df, 22326)
+
+def arl_recommender(rules_df, product_id, rec_count=1):
+    sorted_rules = rules_df.sort_values("lift", ascending=False)
+    recommendation_list = []
+    for i, product in enumerate(sorted_rules["antecedents"]):
+        for j in list(product):
+            if j == product_id:
+                recommendation_list.append(list(sorted_rules.iloc[i]["consequents"])[0])
+
+    return recommendation_list[0:rec_count]
+
+# Normalde önerilen ilk ürünü alıyorduk. İhtiyaç halinde kullanılması için rec_count parametresini ekledik.
+# Buna göre rec_count ile kaç tane ürün öerilmesini istediğimizi de belirtebiliyoruz.
+
+arl_recommender(rules, 22492, 1)
+arl_recommender(rules, 22492, 2)
+arl_recommender(rules, 22492, 3)
+
+
+
+
+
